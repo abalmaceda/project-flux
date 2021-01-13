91c493480dfa671bc8975812a96213a499fef165
Alvaro Balmaceda <alvaro.balmaceda@carbonobit.com>
2021-01-13 07:20:01 -0300

    Se crea codigo para iniciar projecto pokemon

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # project-flux
 Objetivo es evaluar conocimiento técnico desarrollador full-stack
+
+
+## Project setup
+```
+./init_project.sh
+```
---
+++ init_project.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/abalmaceda/project-pokemon-flux-back.git
+git clone https://github.com/abalmaceda/project-pokemon-flux-front.git
+
+docker-compose build --no-cache
+docker-compose up
